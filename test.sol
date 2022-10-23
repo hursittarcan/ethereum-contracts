2d07e42f2d94fa718fe6b832b742ac11f7264df2
Hursit Tarcan <75273616+hursittarcan@users.noreply.github.com>
2022-10-23 21:32:48 +0200

    Add files via upload

--- test.sol
+++ test.sol
@@ -1,1 +1,9 @@
+pragma solidity >=0.7.0 <0.9.0;
 
+contract Test {
+    string hello = "Hello World!";
+
+    function Hello() public view returns(string memory) {
+        return hello; 
+    } 
+}
